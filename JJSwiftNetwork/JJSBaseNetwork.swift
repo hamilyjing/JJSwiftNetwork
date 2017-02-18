d1ec8bf0f5e260cc105ffee5b5a7bb50d24660b3
JJ <gongjian_001@126.com>
2017-02-18 16:35:49 +0800

    update

--- JJSwiftNetwork/JJSBaseNetwork.swift
+++ JJSwiftNetwork/JJSBaseNetwork.swift
@@ -11,6 +11,9 @@ import UIKit
 import Alamofire
 
 public class JJSBaseNetwork: NSObject {
+    
+    var successCompletionBlock: ((JJSBaseNetwork) -> Void)?
+    var failureCompletionBlock: ((JJSBaseNetwork) -> Void)?
 
     var hostURL: String!
     var pathURL: String?
@@ -20,29 +23,90 @@ public class JJSBaseNetwork: NSObject {
     lazy var httpMethod: HTTPMethod = .get
     
     var response: HTTPURLResponse?
+    var responseStatusCode: Int { return (self.response?.statusCode)! }
+    var responseString: String?
+    var responseError: Error?
     
-    public func start() {
+    var responseHeaders: [String: String] = [:]
+    
+    open func start() {
         let request = Alamofire.request("https://httpbin.org/delete", method: httpMethod, parameters: parameters, encoding: URLEncoding.default, headers: httpHeaders)
         
         let requestComplete: (HTTPURLResponse?, Result<String>) -> Void = { response, result in
+            self.handleRequestResult(response, result)
         }
         
-        if let request = request as? DataRequest {
-            request.responseString { response in
-                requestComplete(response.response, response.result)
-            }
-        } else if let request = request as? DownloadRequest {
-            request.responseString { response in
-                requestComplete(response.response, response.result)
-            }
+        request.responseString { response in
+            requestComplete(response.response, response.result)
         }
     }
     
-    public func stop() {
+    open func stop() {
+        
+    }
+    
+    open func requestCompleteFilter() {
+        
+    }
+    
+    open func requestFailedFilter() {
         
     }
     
     func buildRequestURL() -> String {
         return ""
+    }
+    
+    func handleRequestResult(_ response: HTTPURLResponse?, _ result: Result<String>) {
+        
+        self.responseString = result.value
+        self.responseError = result.error
+        
+        
+        self.response = response
+        if let response = self.response {
+            for (field, value) in response.allHeaderFields {
+                self.responseHeaders["\(field)"] = "\(value)"
+            }
+        }
+        
+        let succeed = checkResult()
+        if succeed {
+            requestCompleteFilter()
+            if let successCompletionBlock = self.successCompletionBlock {
+                successCompletionBlock(self)
+            }
+        } else {
+            requestFailedFilter()
+            if let failureCompletionBlock = self.failureCompletionBlock {
+                failureCompletionBlock(self)
+            }
+        }
+        
+        clearCompletionBlock()
+    }
+    
+    func checkResult() -> Bool {
+        let result = statusCodeValidator()
+        if !result {
+            return result
+        }
+        
+        return result
+        // jsonValidator
+    }
+    
+    func statusCodeValidator() -> Bool {
+        let statusCode = self.responseStatusCode
+        if statusCode >= 200 && statusCode <= 299 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func clearCompletionBlock() {
+        successCompletionBlock = nil
+        failureCompletionBlock = nil
     }
 }
