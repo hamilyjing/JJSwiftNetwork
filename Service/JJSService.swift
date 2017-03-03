76f055ad17eb78cdf39f5a97ff692cdfe46bb970
JJ <gongjian_001@126.com>
2017-03-03 13:58:00 +0800

    update

---
+++ Service/JJSService.swift
@@ -0,0 +1,24 @@
+//
+//  JJSService.swift
+//  JJSwiftNetwork
+//
+//  Created by JJ on 3/3/17.
+//  Copyright © 2017 jianjing. All rights reserved.
+//
+
+import UIKit
+
+open class JJSService: NSObject {
+    
+    open func startRequest(request: JJSNetwork) {
+        
+    }
+    
+    open func responseCallBack() {
+        
+    }
+    
+    open func postResponseNotification() {
+        
+    }
+}
